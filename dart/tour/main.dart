54b0b1a557c5e73cb4f1ad49ca2f8b99de4ec732
yinshuwei <yinshuwei@foxmail.com>
2019-03-04 00:12:30 +0800

    dio

--- dart/tour/main.dart
+++ dart/tour/main.dart
@@ -1,7 +1,16 @@
 import 'package:dio/dio.dart';
+import 'dart:async';
 
 main() async {
+  Dio dio = new Dio();
   Response response;
-response=await dio.get("http://www.freshfresh.com")
-print(response.data.toString());
+  response = await dio.get("http://www.freshfresh.com");
+  print(response.data.toString());
+  response = await dio.get("http://wx.freshfresh.com");
+  print(response.data.toString());
+
+  var result = await Future.delayed(new Duration(seconds: 1), () {
+    return "OK";
+  });
+  print(result);
 }
